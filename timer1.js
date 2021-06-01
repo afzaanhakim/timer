1dbff74b277065dc989fa44ad80d2c0c0259a570
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-01 21:26:26 +0000

    refactored conditions using isNan function

--- timer1.js
+++ timer1.js
@@ -2,11 +2,11 @@ const args = process.argv.slice(2);
 
 const timer = (args) => {
   for (let i = 0; i < args.length; i++) {
-    if (args[i] !== NaN && args[i] > 0) {
+    if (!isNaN(args[i]) && args[i] > 0) {
       setTimeout(() => {
         process.stdout.write('\x07');
-      }, args[i] * 1000)
-    } 
+      }, args[i] * 1000);
+    }
   }
 };
 
