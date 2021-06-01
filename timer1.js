626f79bb653a162eae68c553baa7386fc1bd06da
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-01 21:23:05 +0000

    added a condition to check for test cases and only call setTimeout once those conditions are passed

--- timer1.js
+++ timer1.js
@@ -1,19 +1,14 @@
-const args = process.argv.slice(2)
+const args = process.argv.slice(2);
 
 const timer = (args) => {
-let numOfBeeps = [];
-for (i = 0; i < args.length; i++) {
-numOfBeeps.push(Number (args[i]))
-}
-// console.log(numOfBeeps)
-  for (let i = 0; i < numOfBeeps.length; i++){
-
-    setTimeout(() => {
-      process.stdout.write('\x07');
-    }, numOfBeeps[i] * 1000)
-  } 
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] !== NaN && args[i] > 0) {
+      setTimeout(() => {
+        process.stdout.write('\x07');
+      }, args[i] * 1000)
+    } 
+  }
 };
 
 timer(args);
 
-//currently it is beeping incrementally at 3rd second, 5th second, 9th second, 10th second, 15th second. I am unsure if it is supposed to do that or it should be beeping at those seconds
